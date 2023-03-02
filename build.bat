711b4c701815d5c48fdd77b0b4e6608e36bdb088
Truong Xuan Doan <dtruongxuan.cdt@gmail.com>
2023-03-03 06:35:27 +0700

    modify build.bat

--- build.bat
+++ build.bat
@@ -3,9 +3,9 @@
 set NAME=nodumbcafe
 
 if "%1"=="appbg" (
-    pyinstaller --name %NAME% --onefile --noconsole --icon .\icon\nocf.ico .\autolockscreen_bg.py .\avoiddumbcafe.spec
+    pyinstaller --name %NAME% --onefile --noconsole --icon .\icon\nocf.ico .\autolockscreen_bg.py .\nodumbcafe.spec
 ) else if "%1"=="app" (
-    pyinstaller --name %NAME% --onefile --icon .\icon\nocf.ico .\autolockscreen.py .\avoiddumbcafe.spec
+    pyinstaller --name %NAME% --onefile --icon .\icon\nocf.ico .\autolockscreen.py .\nodumbcafe.spec
 ) else (
     echo Usage: build.bat "[appbg|app]" 
     echo "appbg: build an app running in background" 
